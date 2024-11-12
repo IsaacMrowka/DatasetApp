9164497bad90001bb83c8c4c5e5cc013d8fcd475
IsaacMrowka <i.mrowka12@gmail.com>
2024-11-11 17:39:17 -0800

    shingling specific columns

--- Project.py
+++ Project.py
@@ -1,20 +1,29 @@
 import pandas as pd
 
+# Load the CSV file
 def openfile(inputfile):
     data = pd.read_csv(inputfile, encoding='utf-8')
-    data = str(data).lower().replace(" ", "")
     return data
 
-def generate_shingles(data, k):
-    n = len(data)
+#normalize strings and remove empty spaces
+def preprocess(data):
+    data = data.fillna('N/A')
+    data = data[['Category', 'Country', 'Product.Name']] 
+    data = data.replace(' ', '')
+    for col in data.columns:
+        data[col] = data[col].str.lower()
+    return data
+
+def generate_shingles(text, k):
+    n = len(text)
     shingles = []
-    for i in range(n-k+1):
-        shingle = ''.join(data[i:i+k])
+    for i in range(n - k + 1):
+        shingle = text[i:i + k]
         shingles.append(shingle)
     return shingles
 
-data = openfile('superstore.csv')
-shingled_data = generate_shingles(data, 3)
+data = openfile('superstore.csv') 
+prep_data = preprocess(data) 
 
-print(shingled_data)
-
+product_shingles = prep_data['Product.Name'].apply(lambda x: generate_shingles(x, 3))
+print(product_shingles.head())
